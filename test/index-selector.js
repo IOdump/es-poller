2bd08991873af534635743a3bf5b3d0e1fc28d23
Mathieu Elie <mathieuelie@gmail.com>
2014-01-04 18:50:51 +0100

    index selector get latest index in logstash

--- test/index-selector.js
+++ test/index-selector.js
@@ -15,4 +15,23 @@ var IndexSelector = rewire('../lib/index-selector');
     done();
   });
 
+
+  it("should select latest logstash index", function(done) {
+
+    var selector = new IndexSelector();
+    
+    // logstash-2014.01.03  == expected example 
+
+    var dateFormat = require('dateformat');
+    var now = new Date();
+    var expected = 'logstash-' + dateFormat(now, 'yyyy.mm.dd');
+
+
+    var index = selector.latest();
+    
+    expect(index).to.be.expected;
+
+    done();
+  });
+
 });
