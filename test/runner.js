9d5c277cda36546b4b0abd54e68a12959bf31b5c
Mathieu Elie <mathieuelie@gmail.com>
2014-01-04 20:37:57 +0100

    test on poll function

--- test/runner.js
+++ test/runner.js
@@ -38,4 +38,23 @@ var Runner = rewire('../lib/runner');
     done();
   });
 
+  it("should call search on elasticlient on poll", function(done) {
+
+    var fake_amqp_connection = {exchange: function() {return {}; } };
+    var fake_client = { };
+
+    var callback = sinon.stub().returns(
+      {  "then": function () {} }
+    );
+
+    fake_client.search = callback;
+
+    var runner = new Runner(fake_client, fake_amqp_connection, {});
+    runner.poll();
+
+    sinon.assert.calledOnce(callback);
+
+    done();
+  });
+
 });
