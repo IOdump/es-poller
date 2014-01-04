06a604b5a8921ab162691569a00a32a4543e6480
Mathieu Elie <mathieuelie@gmail.com>
2014-01-04 20:19:59 +0100

    test runner run method

--- test/runner.js
+++ test/runner.js
@@ -1,7 +1,12 @@
 var expect = require('chai').expect
+, chai      = require('chai')
 , sinon      = require('sinon')
-, sinon      = require('should')
+, sinonChai      = require('sinon-chai')
+, should      = require('should')
 , rewire = require('rewire');
+
+
+chai.use(sinonChai);
 
 var Runner = rewire('../lib/runner');
 
@@ -13,6 +18,23 @@ var Runner = rewire('../lib/runner');
     var runner = new Runner({}, fake_amqp_connection, {});
 
     expect(runner).to.not.be.undefined;
+    done();
+  });
+
+  it("should call setInterval on run", function(done) {
+
+    var fake_amqp_connection = {exchange: function() {return {}; } };
+    var runner = new Runner({}, fake_amqp_connection, {});
+
+    var callback = sinon.spy();
+
+    // mocking inside the required module with rewire
+    Runner.__set__("setInterval", callback);
+
+    runner.run();
+
+    sinon.assert.calledOnce(callback);
+
     done();
   });
 
